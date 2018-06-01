05d5e5abc7cfdf61dad54a5b7cd3a27b2c6faf8a
mmnarola <mm@narola.email>
2018-06-01 16:31:57 +0530

    change

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -68,7 +68,7 @@ router.put("/", async (req, res) => {
     }
 });
 
-router.get("/user_list", async (req, res) => {
+router.get("/", async (req, res) => {
     user_id = req.userInfo.id;
     var resp_data = await user_helper.get_user_by_id(user_id);
     if (resp_data.status == 0) {
