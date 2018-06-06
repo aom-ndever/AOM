97fd5b28a9cf397f9a7a3ef8f1e3bee58e21dda8
mmnarola <mm@narola.email>
2018-06-06 16:30:04 +0530

    change

--- server/routes/artist/track.js
+++ server/routes/artist/track.js
@@ -46,6 +46,9 @@ router.post("/", async (req, res) => {
     if (req.body.price && req.body.price != null) {
         obj.price = req.body.price;
     }
+    if (req.body.music_type && req.body.music_type != null) {
+        obj.music_type = req.body.music_type;
+    }
 
     async.waterfall([
         function (callback) {
