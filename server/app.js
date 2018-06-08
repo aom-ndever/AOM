f417afcbbeac06dea67239339909e51663e09611
sisnarola <sis@narola.email>
2018-06-08 10:50:32 +0530

    minor change

--- server/app.js
+++ server/app.js
@@ -113,9 +113,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.set('port', process.env.PORT || 3000);
-var server = app.listen(app.get('port'), function() {
-  debug('Express server listening on port ' + server.address().port);
-});
-
 module.exports = app;
