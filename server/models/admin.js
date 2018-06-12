249c716351925499c4f8bc5fd7cc3c8da920b4fc
mmnarola <mm@narola.email>
2018-06-12 10:34:29 +0530

    commit model

---
+++ server/models/admin.js
@@ -0,0 +1,21 @@
+//Require Mongoose
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
+
+//Define a schema
+var Schema = mongoose.Schema;
+
+var AdminModelSchema = new Schema({
+    first_name: String,
+    last_name: String,
+    account_type: String,
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    refresh_token: { type: String },
+    created_at: { type: Date, default: Date.now }
+}, { versionKey: false });
+
+var Admin = mongoose.model('admin', AdminModelSchema, 'admin');
+
+module.exports = Admin;
