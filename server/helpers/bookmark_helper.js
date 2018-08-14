671b48392783ab1f78b62470a654796e2d65f6c7
mmnarola <mm@narola.email>
2018-08-14 10:14:12 +0530

    commit for bookmark insert

--- server/helpers/bookmark_helper.js
+++ server/helpers/bookmark_helper.js
@@ -1,10 +1,10 @@
-var Bookmark = require("./../models/bookmark_artist");
+var Bookmark = require("../models/bookmark");
 var bookmark_helper = {};
 var mongoose = require('mongoose');
 var ObjectId = mongoose.Types.ObjectId;
 
 
-bookmark_helper.insert_book_mark_artist = async (object) => {
+bookmark_helper.insert_book_mark_track = async (object) => {
     let bookmark = new Bookmark(object)
     try {
         let data = await bookmark.save();
@@ -15,28 +15,26 @@ bookmark_helper.insert_book_mark_artist = async (object) => {
 };
 
 
-bookmark_helper.get_all_bookmarked_artist = async (user_id) => {
+bookmark_helper.get_all_bookmarked_track = async (user_id, track_id) => {
     try {
-        var artist = await Bookmark
-            .find({ "user_id": new ObjectId(user_id) })
-            .populate('artist')
-        if (artist) {
-            return { "status": 1, "message": "Artist details found", "artist": artist };
+        var bookmark = await Bookmark
+            .findOne({ "user_id": new ObjectId(user_id), "track_id": new ObjectId(track_id) })
+            .populate('track_id')
+
+        if (bookmark) {
+            return { "status": 1, "message": "bookmark details found", "bookmark": bookmark };
         } else {
-            return { "status": 2, "message": "Artist not found" };
+            return { "status": 2, "message": "bookmark not found" };
         }
     } catch (err) {
-        return { "status": 0, "message": "Error occured while finding artist", "error": err }
+        return { "status": 0, "message": "Error occured while finding bookmark", "error": err }
     }
 };
 
 
-
-
-
-bookmark_helper.delete_bookmark = async (user_id, bookmark_id) => {
+bookmark_helper.delete_bookmark = async (bookmark_id) => {
     try {
-        let user = await Bookmark.findOneAndRemove({ user_id: new ObjectId(user_id), _id: new ObjectId(bookmark_id) });
+        let user = await Bookmark.findOneAndRemove({ _id: new ObjectId(bookmark_id) });
         if (!user) {
             return { status: 2, message: "Record has not Deleted" };
         } else {
@@ -47,4 +45,25 @@ bookmark_helper.delete_bookmark = async (user_id, bookmark_id) => {
     }
 };
 
+
+bookmark_helper.get_all_bookmarked_tracks = async (user_id, start, length) => {
+    try {
+        var bookmark = await Bookmark
+            .find({ "user_id": new ObjectId(user_id) })
+            .populate({ path: 'track_id', populate: { path: 'artist_id' } })
+            .populate({ path: 'user_id', populate: { path: 'music_type' } })
+            .skip(start)
+            .limit(length)
+
+
+
+        if (bookmark) {
+            return { "status": 1, "message": "bookmark details found", "bookmark": bookmark };
+        } else {
+            return { "status": 2, "message": "bookmark not found" };
+        }
+    } catch (err) {
+        return { "status": 0, "message": "Error occured while finding bookmark", "error": err }
+    }
+};
 module.exports = bookmark_helper;
