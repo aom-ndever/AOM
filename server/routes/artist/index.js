fc6530cfe15fbeac079a7cab7ff961f68670babc
mmnarola <mm@narola.email>
2018-09-04 12:06:07 +0530

    commit

--- server/routes/artist/index.js
+++ server/routes/artist/index.js
@@ -360,7 +360,7 @@ router.delete('/bank/:bank_id', async (req, res) => {
     } else if (del_resp.status === 2) {
         res.status(config.BAD_REQUEST).json({ "status": 0, "message": "Can't delete artist bank" });
     } else {
-        res.status(config.OK_STATUS).json({ "status": 1, "message": "artist bank has been deleted" });
+        res.status(config.OK_STATUS).json({ "status": 1, "message": " Artist bank account has been deleted" });
     }
 });
 
