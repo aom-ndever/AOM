8aca55db022079bd8bec32800298ae559cf7338d
mmnarola <mm@narola.email>
2018-09-04 14:12:15 +0530

    commit

--- server/helpers/artist_helper.js
+++ server/helpers/artist_helper.js
@@ -84,7 +84,7 @@ artist_helper.insert_user_to_artists = async (object) => {
         let data = await Artist.insertMany([object]);
         return { "status": 1, "message": "Record inserted", "artist": data };
     } catch (err) {
-        return { "status": 0, "message": "Already Registered as artist by This Email" };
+        return { "status": 0, "message": "Already registered as artist by this email Id" };
     }
 };
 
