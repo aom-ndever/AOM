24e19547fd583e0c36d9729a110dc29de1c42412
mmnarola <mm@narola.email>
2018-09-04 12:33:47 +0530

    commit

--- server/helpers/artist_helper.js
+++ server/helpers/artist_helper.js
@@ -83,7 +83,7 @@ artist_helper.insert_user_to_artists = async (object) => {
         let data = await Artist.insertMany([object]);
         return { "status": 1, "message": "Record inserted", "artist": data };
     } catch (err) {
-        return { "status": 0, "message": "Error occured while inserting artist", "error": err };
+        return { "status": 0, "message": "Already Registered by This Email" };
     }
 };
 
