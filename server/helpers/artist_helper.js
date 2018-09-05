4b5c6146a35e957a23eaee1e9bf6123c5795bb1e
mmnarola <mm@narola.email>
2018-09-05 14:53:26 +0530

    commit

--- server/helpers/artist_helper.js
+++ server/helpers/artist_helper.js
@@ -421,64 +421,64 @@ artist_helper.get_all_artist = async () => {
 artist_helper.get_artist_by_filter = async (filter, start, length, filters) => {
     try {
 
-        // var artist = await Artist
-        //     .find({ "flag": false })
-        //     .populate('music_type')
-        //     .populate('state')
-        //     .skip(start)
-        //     .limit(length)
-        var aggregate = [
-            {
-                "$match": {
-                    "flag": false
-                }
-            },
+        var artist = await Artist
+            .find({ "flag": false })
+            .populate('music_type')
+            .populate('state')
+            .skip(start)
+            .limit(length)
+        // var aggregate = [
+        //     {
+        //         "$match": {
+        //             "flag": false
+        //         }
+        //     },
 
-            {
-                '$lookup': {
-                    from: 'music_type',
-                    localField: 'music_type',
-                    foreignField: '_id',
-                    as: 'music_type'
-                }
-            },
-            {
-                '$unwind': '$music_type'
-            },
-            {
-                '$lookup': {
-                    from: 'state',
-                    localField: 'state',
-                    foreignField: '_id',
-                    as: 'state'
-                }
-            },
-            {
-                '$unwind': '$state'
-            },
+        //     {
+        //         '$lookup': {
+        //             from: 'music_type',
+        //             localField: 'music_type',
+        //             foreignField: '_id',
+        //             as: 'music_type'
+        //         }
+        //     },
+        //     {
+        //         '$unwind': '$music_type'
+        //     },
+        //     {
+        //         '$lookup': {
+        //             from: 'state',
+        //             localField: 'state',
+        //             foreignField: '_id',
+        //             as: 'state'
+        //         }
+        //     },
+        //     {
+        //         '$unwind': '$state'
+        //     },
 
-            {
-                $skip: start
-            },
-            {
-                $limit: length
-            }
-        ];
+        //     {
+        //         $skip: start
+        //     },
+        //     {
+        //         $limit: length
+        //     }
+        // ];
 
-        if (filter) {
-            aggregate.push({
-                "$match": filter
-            })
-        }
+        // if (filter) {
+        //     aggregate.push({
+        //         "$match": filter
+        //     })
+        // }
 
-        if (filters) {
-            aggregate.push({
-                "$match":
+        // if (filters) {
+        //     aggregate.push({
+        //         "$match":
 
-                    { $or: [{ "artist.first_name": filters }, { "artist.last_name": filters }] }
-            });
-        }
-        let artist = await Artist.aggregate(aggregate);
+        //             { $or: [{ "artist.first_name": filters }, { "artist.last_name": filters }] }
+        //     });
+        // }
+        // let artist = await Artist.aggregate(aggregate);
 
         if (artist) {
             return { "status": 1, "message": "artist details found", "artist": artist };
