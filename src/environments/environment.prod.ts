bd8a5ad43215a2d11a697d9cd824f46eb74b32f5
hda-narola <hda@narola.email>
2019-06-04 17:35:43 +0530

    Changed client id and client secret

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,10 @@
 export const environment = {
   production: true,
   GMAP_KEY: 'AIzaSyD6DEW05AQ8M_hrpSI7eTE3jbZB9k7c0Oc',
-  GOOGLE_CLIENT_ID: '112936390114-hb3eeajuhemp9ls1n4i7cag8apf07mf8.apps.googleusercontent.com',
-  GOOGLE_CLIENT_SECRET: 'bohGfi5VZz7Yd4kGgWkUJimF',
+  // GOOGLE_CLIENT_ID: '112936390114-hb3eeajuhemp9ls1n4i7cag8apf07mf8.apps.googleusercontent.com',
+  // GOOGLE_CLIENT_SECRET: 'bohGfi5VZz7Yd4kGgWkUJimF',
+  GOOGLE_CLIENT_ID: '376011943773-v60garkus5cc78ca6cphrp6bj7mgh69o.apps.googleusercontent.com',
+  GOOGLE_CLIENT_SECRET: 'NwQBTXbJ5PqhUAQGM13nzzlq',
   // API_URL: 'http://localhost:3000',
   socketUrl: 'https://aom.ndever.nyc:3000',
   API_URL: 'https://aom.ndever.nyc:3000',
