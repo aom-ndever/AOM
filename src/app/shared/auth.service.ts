543bd1bc3e8dff358beb1dfb84728586568c71e8
knvishal <vik@narola.email>
2021-03-06 13:02:28 +0530

    Fixes routing issue.

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,23 +1,30 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from "@angular/router";
 
 @Injectable()
 export class AuthService implements CanActivate {
   public authToken;
   private isAuthenticated = true; // Set this value dynamically
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const user = localStorage.getItem('user');
-    if (user) {
-      return true;
-    }
-    if (state.url.includes('/admin')) {
-      this.router.navigate(['/admin/login']);
-    } else if (state.url.includes('/admin') && !user.hasOwnProperty('admin')) {
-      this.router.navigate(['']);
+    const user = JSON.parse(localStorage.getItem("user"));
+    console.log(" : user ==> ", user);
+
+    console.log(" : test ==>> ", state.url.includes("/admin"), user);
+
+    if (state.url.includes("/admin") && user && !user.hasOwnProperty("admin")) {
+      this.router.navigate([""]);
+    } else if (state.url.includes("/admin")) {
+      console.log(" : here ==> ");
+      this.router.navigate(["/admin/login"]);
     } else {
-      this.router.navigate(['']);
+      this.router.navigate([""]);
     }
     return false;
   }
