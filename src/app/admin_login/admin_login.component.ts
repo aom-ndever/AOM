fc6a9c1ac77a95040b628c84f80fcccb2b369498
knvishal <vik@narola.email>
2021-03-06 14:46:05 +0530

    Changes in auth service

--- src/app/admin_login/admin_login.component.ts
+++ src/app/admin_login/admin_login.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { AdminLoginService } from './admin_login.service';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router';
-import { BsModalService } from 'ngx-bootstrap/modal';
-import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { Component, OnInit } from "@angular/core";
+import { AdminLoginService } from "./admin_login.service";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from "@angular/forms";
+import { ToastrService } from "ngx-toastr";
+import { Router } from "@angular/router";
+import { BsModalService } from "ngx-bootstrap/modal";
+import { BsModalRef } from "ngx-bootstrap/modal/bs-modal-ref.service";
 
 @Component({
-  selector: 'app-admin_login',
-  templateUrl: './admin_login.component.html',
-  styleUrls: []
+  selector: "app-admin_login",
+  templateUrl: "./admin_login.component.html",
+  styleUrls: [],
 })
 export class AdminLoginComponent implements OnInit {
+  user: any;
   ModelRef: BsModalRef;
   admin_data: any = {};
   show_spinner: Boolean = false;
@@ -27,59 +33,76 @@ export class AdminLoginComponent implements OnInit {
     private modalService: BsModalService
   ) {
     this.admin_formgroup = this.fb.group({
-      'email': ['', [Validators.required, Validators.email]],
-      'password': ['', [Validators.required]],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]],
     });
     this.forget_formgroup = this.fb.group({
-      email: ['', [Validators.required, Validators.email, this.noWhitespaceValidator]]
+      email: [
+        "",
+        [Validators.required, Validators.email, this.noWhitespaceValidator],
+      ],
     });
-  }
-
-  noWhitespaceValidator(control: FormControl) {
-    if (typeof (control.value || '') === 'string' || (control.value || '') instanceof String) {
-      const isWhitespace = (control.value || '').trim().length === 0;
-      const isValid = !isWhitespace;
-      return isValid ? null : { 'whitespace': true };
+    this.user = JSON.parse(localStorage.getItem("user"));
+    if (this.user && this.user.hasOwnProperty("admin")) {
+      this.router.navigate(["/admin/home"]);
     }
   }
 
-  ngOnInit() { }
+  noWhitespaceValidator(control: FormControl) {
+    if (
+      typeof (control.value || "") === "string" ||
+      (control.value || "") instanceof String
+    ) {
+      const isWhitespace = (control.value || "").trim().length === 0;
+      const isValid = !isWhitespace;
+      return isValid ? null : { whitespace: true };
+    }
+  }
+
+  ngOnInit() {}
 
   signin() {
     this.show_spinner = true;
-    this.adminLoginService.login(this.admin_data).subscribe(response => {
-      localStorage.setItem('user', JSON.stringify(response));
-      this.router.navigate(['/admin/home']);
-    }, error => {
-      this.toastr.error(error['error'].message, 'Error!');
-      this.show_spinner = false;
-    }, () => {
-      this.show_spinner = false;
-    });
+    this.adminLoginService.login(this.admin_data).subscribe(
+      (response) => {
+        localStorage.setItem("user", JSON.stringify(response));
+        this.router.navigate(["/admin/home"]);
+      },
+      (error) => {
+        this.toastr.error(error["error"].message, "Error!");
+        this.show_spinner = false;
+      },
+      () => {
+        this.show_spinner = false;
+      }
+    );
   }
 
   openContestModel(template: any) {
     this.forget_data = {};
     this.forget_validation = false;
-    this.ModelRef = this.modalService.show(template, { backdrop: 'static' });
+    this.ModelRef = this.modalService.show(template, { backdrop: "static" });
   }
 
   // Forget password
   forgetPassword(flag) {
     if (flag) {
       this.show_spinner = true;
-      this.adminLoginService.forgetPassword(this.forget_data).subscribe((response) => {
-        this.forget_data = {};
-        this.ModelRef.hide();
-        this.toastr.success(response['message'], 'Success!');
-      }, (error) => {
-        this.toastr.error(error['error'].message, 'Error!');
-        this.show_spinner = false;
-      }, () => {
-        this.show_spinner = false;
-      });
+      this.adminLoginService.forgetPassword(this.forget_data).subscribe(
+        (response) => {
+          this.forget_data = {};
+          this.ModelRef.hide();
+          this.toastr.success(response["message"], "Success!");
+        },
+        (error) => {
+          this.toastr.error(error["error"].message, "Error!");
+          this.show_spinner = false;
+        },
+        () => {
+          this.show_spinner = false;
+        }
+      );
     }
     this.forget_validation = !flag;
   }
-
 }
--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -13,18 +13,32 @@ export class AuthService implements CanActivate {
 
   constructor(private router: Router) {}
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const user = localStorage.getItem("user");
-    if (user) {
-      return true;
-    }
+    const user = JSON.parse(localStorage.getItem("user"));
+    console.log(" : user ==> ", user);
+
+    console.log(
+      ' : state.url.includes("/admin") ==> ',
+      state.url.includes("/admin")
+    );
+    console.log(
+      ' : user.hasOwnProperty("admin") ==> ',
+      user.hasOwnProperty("admin")
+    );
 
     if (state.url.includes("/admin")) {
-      this.router.navigate(["/admin/login"]);
+      if (user.hasOwnProperty("admin")) {
+        return true;
+      } else {
+        this.router.navigate(["/admin/login"]);
+      }
     } else if (state.url.includes("/admin") && !user.hasOwnProperty("admin")) {
       this.router.navigate([""]);
     } else {
       this.router.navigate([""]);
     }
+    // if (user) {
+    //   return true;
+    // }
     return false;
   }
 }
